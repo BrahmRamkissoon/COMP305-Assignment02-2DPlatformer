845a20ada2a0373bb0f44bc2e481c92f2e750cf3
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-26 02:14:39 -0400

    added coin animation clip and controller, coin destroy on player contact, sound

---
+++ Assets/_Scripts/CoinController.cs
@@ -0,0 +1,28 @@
+﻿// Filename: CoinController.cs
+// Author: Brahm Ramkissoon
+// Created Date  (dd/mm/yyyy): 26/10/2015
+// Description: Make coin dissappear when coin is in contact with player
+using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Make coin disappear when coin is in contact with player
+    void OnCollisionEnter2D ( Collision2D otherCollider )
+    {
+        if ( otherCollider.gameObject.CompareTag("Player") )
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿// Filename: PlayerController.cs
+// Author: Brahm Ramkissoon
+// Created Date  (dd/mm/yyyy): 26/10/2015
+// Description: all things controlled by player sprite
+// Revision:        - added coin sound
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // PRIVATE INSTANCE VARIABLES
+    private AudioSource[] _audioSources;
+    private AudioSource _coinSound;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    this._audioSources = gameObject.GetComponents<AudioSource>();
+	    this._coinSound = this._audioSources[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Play sound on collision with coin
+    void OnCollisionEnter2D ( Collision2D otherCollider )
+    {
+        if ( otherCollider.gameObject.CompareTag("Coin") )
+        {
+            this._coinSound.Play();
+        }
+    }
+}
